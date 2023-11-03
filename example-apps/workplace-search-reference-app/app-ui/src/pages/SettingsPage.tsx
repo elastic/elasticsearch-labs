25c45cf2541d1299b5fef1dd5cc0e10765d18ac9
Tim Grein <tim.grein@elastic.co>
2023-11-03 13:52:47 +0100

    Add the settings page

---
+++ example-apps/workplace-search-reference-app/app-ui/src/pages/SettingsPage.tsx
@@ -0,0 +1,11 @@
+import SearchApplicationSettings from "../components/SearchApplicationSettings";
+
+export default function SettingsPage() {
+    return (
+        <div className="flex justify-center min-h-screen">
+            <div className="w-full max-w-2xl">
+                <SearchApplicationSettings />
+            </div>
+        </div>
+    );
+}
