6ece101a523e9cefdd922c2b8a717d0e1b714a57
István Zoltán Szabó <szabosteve@gmail.com>
2024-03-12 14:15:06 +0100

    Relocates notebook.

--- bin/find-notebooks-to-test.sh
+++ bin/find-notebooks-to-test.sh
@@ -3,7 +3,6 @@
 EXEMPT_NOTEBOOKS=(
     "notebooks/search/07-inference.ipynb"
     "notebooks/search/08-learning-to-rank.ipynb"
-    "notebooks/search/09-inference-cohere.ipynb"
     "notebooks/langchain/langchain-vector-store.ipynb"
     "notebooks/langchain/self-query-retriever-examples/chatbot-example.ipynb"
     "notebooks/langchain/self-query-retriever-examples/chatbot-with-bm25-only-example.ipynb"
@@ -13,6 +12,7 @@ EXEMPT_NOTEBOOKS=(
     "notebooks/generative-ai/question-answering.ipynb"
     "notebooks/generative-ai/chatbot.ipynb"
     "notebooks/integrations/amazon-bedrock/langchain-qa-example.ipynb"
+    "notebooks/integrations/cohere/inference-cohere.ipynb"
     "notebooks/integrations/llama-index/intro.ipynb"
     "notebooks/integrations/gemini/vector-search-gemini-elastic.ipynb"
     "notebooks/integrations/gemini/qa-langchain-gemini-elasticsearch.ipynb"
