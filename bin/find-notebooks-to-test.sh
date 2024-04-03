b10e91bf08b66bc4de6cce0c29dbf2ed998ecb28
Mark Hoy <mark.hoy@elastic.co>
2024-04-03 17:18:55 -0400

    add notebook exemption for CI

--- bin/find-notebooks-to-test.sh
+++ bin/find-notebooks-to-test.sh
@@ -20,6 +20,7 @@ EXEMPT_NOTEBOOKS=(
     "notebooks/integrations/gemini/qa-langchain-gemini-elasticsearch.ipynb"
     "notebooks/integrations/openai/openai-KNN-RAG.ipynb"
     "notebooks/integrations/gemma/rag-gemma-huggingface-elastic.ipynb"
+    "notebooks/enterprise-search/app-search-engine-exporter.ipynb"
 )
 
 ALL_NOTEBOOKS=$(find notebooks -name "*.ipynb" | grep -v "_nbtest" | grep -v ".ipynb_checkpoints" | sort)
