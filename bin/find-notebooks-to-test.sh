35cb4cf907d8f6442c388ed78ad56570883cd1c7
István Zoltán Szabó <szabosteve@gmail.com>
2024-05-09 16:22:42 +0200

    Adds Cohere & Elasticsearch tutorial notebook.

--- bin/find-notebooks-to-test.sh
+++ bin/find-notebooks-to-test.sh
@@ -14,6 +14,7 @@ EXEMPT_NOTEBOOKS=(
     "notebooks/generative-ai/question-answering.ipynb"
     "notebooks/generative-ai/chatbot.ipynb"
     "notebooks/integrations/amazon-bedrock/langchain-qa-example.ipynb"
+    "notebooks/integrations/cohere/cohere-elasticsearch.ipynb"
     "notebooks/integrations/cohere/inference-cohere.ipynb"
     "notebooks/integrations/llama-index/intro.ipynb"
     "notebooks/integrations/gemini/vector-search-gemini-elastic.ipynb"
