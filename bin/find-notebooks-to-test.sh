5bdbee68261eed55b6836ca4e1131cdf86deaaf5
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-05-21 09:42:56 +0100

    Add per-version testing restrictions

--- bin/find-notebooks-to-test.sh
+++ bin/find-notebooks-to-test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# add any notebooks that are currently not testable to the exempt list
+
 EXEMPT_NOTEBOOKS=(
+    # Add any notebooks that are currently not testable to the exempt list
     "notebooks/esql/esql-getting-started.ipynb"
     "notebooks/search/07-inference.ipynb"
     "notebooks/search/08-learning-to-rank.ipynb"
@@ -25,9 +26,46 @@ EXEMPT_NOTEBOOKS=(
     "notebooks/enterprise-search/app-search-engine-exporter.ipynb"
 )
 
+# Per-version testing exceptions
+# use variables named EXEMPT_NOTEBOOKS__{major}_[minor}_{patch} to list
+# notebooks that cannot run on that stack version or older
+# Examples:
+# EXEMPT_NOTEBOOKS__8 for notebooks that must be skipped on all versions 8.x and older
+# EXEMPT_NOTEBOOKS__8_14 for notebooks that must skipped on versions 8.14 and older
+# EXEMPT_NOTEBOOKS__8_13_2 for notebooks that must be skipped on versions 8.13.2 and older
+
+EXEMPT_NOTEBOOKS__8_13=(
+    # Add any notebooks that must be skipped on versions 8.13 or older here
+    "notebooks/search/03-ELSER.ipynb"
+    "notebooks/model-upgrades/upgrading-index-to-use-elser.ipynb"
+    "notebooks/langchain/langchain-vector-store-using-elser.ipynb"
+)
+
+# this function parses a version given as M[.N[.P]] or M[_N[_P]] into a numeric form
+function parse_version { echo "$@" | awk -F'[._]' '{ printf("%02d%02d%02d\n", $1, $2, $3); }'; }
+
+# this is the version CI is running
+ci_version=$(parse_version ${ES_STACK:-99.99})
+
 ALL_NOTEBOOKS=$(find notebooks -name "*.ipynb" | grep -v "_nbtest" | grep -v ".ipynb_checkpoints" | sort)
 for notebook in $ALL_NOTEBOOKS; do
-    if [[ ! "${EXEMPT_NOTEBOOKS[@]}" =~ $notebook ]]; then
+    skip=
+    # check the master exception list
+    if [[ "${EXEMPT_NOTEBOOKS[@]}" =~ $notebook ]]; then
+        skip=yes
+    else
+        # check the versioned exception lists
+        for exempt_key in ${!EXEMPT_NOTEBOOKS__*}; do
+            exempt_version=$(parse_version ${exempt_key/EXEMPT_NOTEBOOKS__/})
+            if [ $exempt_version -ge $ci_version ]; then
+                exempt_notebooks=$(eval 'echo ${'${exempt_key}'[@]}')
+                if [[ "${exempt_notebooks[@]}" =~ $notebook ]]; then
+                    skip=yes
+                fi
+            fi
+        done
+    fi
+    if [[ "$skip" == "" ]]; then
         echo $notebook
     fi
 done
