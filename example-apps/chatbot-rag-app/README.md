b348c73681a5a814337aef2e94d0cffdf7fa8171
Adrian Cole <adrian.cole@elastic.co>
2024-12-27 17:07:53 +0800

    less AWS properties
    Signed-off-by: Adrian Cole <adrian.cole@elastic.co>

--- example-apps/chatbot-rag-app/README.md
+++ example-apps/chatbot-rag-app/README.md
@@ -67,6 +67,12 @@ docker compose up --build --force-recreate
 *Note*: First time creating the index can fail on timeout. Wait a few minutes
 and retry.
 
+Clean up when finished, like this:
+
+```bash
+docker compose down
+```
+
 ### Run locally
 
 If you want to run this example with Python and Node.js, you need to do a few
---
+++ example-apps/chatbot-rag-app/llm_integrations.py
@@ -0,0 +1,69 @@
+import os
+
+import vertexai
+from langchain_aws import ChatBedrock
+from langchain_cohere import ChatCohere
+from langchain_google_vertexai import ChatVertexAI
+from langchain_mistralai import ChatMistralAI
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
+
+LLM_TYPE = os.getenv("LLM_TYPE", "openai")
+
+
+def init_openai_chat(temperature):
+    return ChatOpenAI(model=os.getenv("CHAT_MODEL"), streaming=True, temperature=temperature)
+
+
+def init_vertex_chat(temperature):
+    VERTEX_PROJECT_ID = os.getenv("VERTEX_PROJECT_ID")
+    VERTEX_REGION = os.getenv("VERTEX_REGION", "us-central1")
+    vertexai.init(project=VERTEX_PROJECT_ID, location=VERTEX_REGION)
+    return ChatVertexAI(streaming=True, temperature=temperature)
+
+
+def init_azure_chat(temperature):
+    return AzureChatOpenAI(model=os.getenv("CHAT_DEPLOYMENT"), streaming=True, temperature=temperature)
+
+
+def init_bedrock(temperature):
+    return ChatBedrock(model_id=os.getenv("CHAT_MODEL"), streaming=True, model_kwargs={"temperature": temperature})
+
+
+def init_mistral_chat(temperature):
+    MISTRAL_API_ENDPOINT = os.getenv("MISTRAL_API_ENDPOINT")
+    MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+    MISTRAL_MODEL = os.getenv("MISTRAL_MODEL", "Mistral-large")
+    kwargs = {
+        "mistral_api_key": MISTRAL_API_KEY,
+        "temperature": temperature,
+    }
+    if MISTRAL_API_ENDPOINT:
+        kwargs["endpoint"] = MISTRAL_API_ENDPOINT
+    if MISTRAL_MODEL:
+        kwargs["model"] = MISTRAL_MODEL
+    return ChatMistralAI(**kwargs)
+
+
+def init_cohere_chat(temperature):
+    COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+    COHERE_MODEL = os.getenv("COHERE_MODEL")
+    return ChatCohere(cohere_api_key=COHERE_API_KEY, model=COHERE_MODEL, temperature=temperature)
+
+
+MAP_LLM_TYPE_TO_CHAT_MODEL = {
+    "azure": init_azure_chat,
+    "bedrock": init_bedrock,
+    "openai": init_openai_chat,
+    "vertex": init_vertex_chat,
+    "mistral": init_mistral_chat,
+    "cohere": init_cohere_chat,
+}
+
+
+def get_llm(temperature=0):
+    if LLM_TYPE not in MAP_LLM_TYPE_TO_CHAT_MODEL:
+        raise Exception(
+            "LLM type not found. Please set LLM_TYPE to one of: " + ", ".join(MAP_LLM_TYPE_TO_CHAT_MODEL.keys()) + "."
+        )
+
+    return MAP_LLM_TYPE_TO_CHAT_MODEL[LLM_TYPE](temperature=temperature)
