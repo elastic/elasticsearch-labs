169396f52cf388078c846d74162996a4c460ac07
Kathleen DeRusso <kathleen.derusso@elastic.co>
2026-07-29 11:42:26 -0400

    Excempt notebooks from CI

--- bin/find-notebooks-to-test.sh
+++ bin/find-notebooks-to-test.sh
@@ -39,7 +39,9 @@ EXEMPT_NOTEBOOKS=(
     "notebooks/integrations/hugging-face/huggingface-integration-millions-of-documents-with-cohere-reranking.ipynb",
     "notebooks/integrations/cohere/updated-cohere-elasticsearch-inference-api.ipynb",
     "notebooks/integrations/alibabacloud-ai-search/inference-alibabacloud-ai-search.ipynb",
-    "notebooks/integrations/jinaai/inference-jinaai.ipynb"
+    "notebooks/integrations/jinaai/inference-jinaai.ipynb",
+    "notebooks/context/manual-walkthrough/part-1/index-metadata-kis.ipynb",
+    "notebooks/context/manual-walkthrough/part-2/index-facts-kis.ipynb"
 )
 
 # Per-version testing exceptions
